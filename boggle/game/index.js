cc0680c4eb77666900e6a2406c49d889cb61bad9
Jeremy Aw <jeremyinelysium@gmail.com>
2020-02-18 14:11:04 +0800

    Check if input is legal

--- boggle/game/index.js
+++ boggle/game/index.js
@@ -78,3 +78,76 @@ const isValidEnglishWord = async word => {
 };
 
 isValidEnglishWord('library');
+const isLegalMove = (boardState, word) => {
+  const startingPoint = word[0];
+  let isLegal = false;
+
+  for (let row = 0; row < GRID_SIZE; row++) {
+    for (let column = 0; column < GRID_SIZE; column++) {
+      // Search for a match for the first letter of given word
+      if (
+        startingPoint.toUpperCase() === boardState[row][column] ||
+        startingPoint === '*'
+      ) {
+        isLegal = wordSearch(boardState, word, 1, row, column);
+      }
+
+      // Terminate if given word is found
+      if (isLegal) {
+        break;
+      }
+    }
+
+    if (isLegal) {
+      break;
+    }
+  }
+
+  return isLegal;
+};
+
+const wordSearch = (boardState, word, wordIndex, currentRow, currentColumn) => {
+  // Terminate when all characters in given word are found
+  if (wordIndex === word.length) {
+    return true;
+  }
+
+  // Ensure that the search is within the board boundaries
+  const rowStart = Math.max(0, currentRow - 1);
+  const rowEnd = Math.min(GRID_SIZE - 1, currentRow + 1);
+  const columnStart = Math.max(0, currentColumn - 1);
+  const columnEnd = Math.min(GRID_SIZE - 1, currentColumn + 1);
+
+
+  const characterToFind = word[wordIndex].toUpperCase();
+  let isWordFound = false;
+
+  // Search for next character in the 8 tiles surrounding the current character
+  for (let row = rowStart; row < rowEnd + 1; row++) {
+    for (let column = columnStart; column < columnEnd + 1; column++) {
+      // Do not check against current character
+      if (row === currentRow && column === currentColumn) {
+        continue;
+      }
+
+      // If next character matches either same character or *, continue search recursively
+      if (
+        boardState[row][column] === characterToFind ||
+        boardState[row][column] === '*'
+      ) {
+        isWordFound = wordSearch(boardState, word, wordIndex + 1, row, column);
+      }
+
+      //Terminate if given word is found
+      if (isWordFound) {
+        break;
+      }
+    }
+
+    if (isWordFound) {
+      break;
+    }
+  }
+
+  return isWordFound;
+};
