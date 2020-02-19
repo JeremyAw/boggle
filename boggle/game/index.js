5a2928f8e8f86ce7b463292600f7da55c302d242
Jeremy Aw <jeremyinelysium@gmail.com>
2020-02-19 15:37:06 +0800

    Insert new game into database when user starts game
    - Connected database to do SQL insert
    - Refactor and clean up variable name and logic
    - Send back correct response
    - Fixed error handling to terminate code execution after any of the responses were sent to ensure server does not crash

--- boggle/game/index.js
+++ boggle/game/index.js
@@ -1,22 +1,23 @@
 const utility = require('./utility');
-const GRID_SIZE = 4;
+const db = require('../database/index.js');
+
+const GRID_SIZE = 4; //todo: shift constant to another file
 
 const createGame = async (req, res) => {
-  const { duration, random, board } = req.body;
-  let boardData;
+  const { duration, random } = req.body;
+  let { board } = req.body;
+
   try {
     if (random) {
-      boardData = utility.generateRandomBoard();
+      board = utility.generateRandomBoard();
     } else {
       // Use default board if user did not provide custom board
       if (board == null || board == undefined) {
-        boardData = await utility.generateDefaultBoard();
+        board = await utility.generateDefaultBoard();
       } else {
         // Check validity of custom board
-        if (utility.isValidBoard(board)) {
-          boardData = board;
-        } else {
-          res
+        if (!utility.isValidBoard(board)) {
+          return res
             .status(400)
             .send(
               `Invalid board given. Board must be a ${GRID_SIZE *
@@ -26,15 +27,35 @@ const createGame = async (req, res) => {
       }
     }
 
+    const token = utility.generateToken();
+    let time_created = new Date().getTime();
+    time_created = Math.floor(time_created / 1000);
+    const points = 0;
+
     const response = {
+      token: token,
       duration: duration,
-      board: boardData
+      board: board
     };
 
-    res.status(201).send(response);
+    const insertSQL = 'INSERT INTO boggle_games VALUES (?, ?, ?, ?, ?, ?)';
+    const insertParams = [null, token, duration, board, time_created, points];
+    db.run(insertSQL, insertParams, function(error) {
+      if (error) {
+        console.log(
+          `Error occurred inserting new game into boggle_games table: ${error}`
+        );
+        throw error;
+      }
+
+      response.id = this.lastID;
+      return res.status(201).send(response);
+    });
   } catch (error) {
     console.log(error);
-    res.status(500).send('An error occurred while processing your request.');
+    return res
+      .status(500)
+      .send('An error occurred while processing your request.');
   }
 };
 
